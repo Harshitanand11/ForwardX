697ff6c82791083da2e287271153469910537de9
Harshitanand11 <83498313+Harshitanand11@users.noreply.github.com>
2023-05-29 20:58:32 +0530

    added firebase to location and notification tab

--- lib/Screens/TabbedScreen.dart
+++ lib/Screens/TabbedScreen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:ui';
-import 'package:myapp/utils.dart';
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 class TabbedScreen extends StatelessWidget {
 @override
@@ -267,20 +264,219 @@ class MachineDetailsScreen extends StatelessWidget {
 // }
 
 
+
 class LocationScreen extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-return Center(
-child: Icon(Icons.location_on, size: 350),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('Location List'),
+      // ),
+      body: LocationListScreen(),
+    );
+  }
 }
 
+class LocationListScreen extends StatefulWidget {
+  @override
+  _LocationListScreenState createState() => _LocationListScreenState();
+}
+
+class _LocationListScreenState extends State<LocationListScreen> {
+  CollectionReference locationsCollection =
+  FirebaseFirestore.instance.collection('locations');
+
+  List<Location> locations = [];
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    loadLocations();
+  }
+
+  Future<void> loadLocations() async {
+    if (isLoading) return;
+    setState(() {
+      isLoading = true;
+    });
+
+    // Simulating API call or data retrieval
+    await Future.delayed(Duration(seconds: 2));
+
+    QuerySnapshot querySnapshot = await locationsCollection.get();
+
+    List<Location> newLocations = querySnapshot.docs.map((doc) {
+      return Location(
+        doc['locationName'],
+        doc['machineName'],
+        doc['status'],
+        doc['description'],
+      );
+    }).toList();
+
+    setState(() {
+      locations.addAll(newLocations);
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading
+        ? Center(child: CircularProgressIndicator())
+        : ListView.builder(
+      itemCount: locations.length,
+      itemBuilder: (context, index) {
+        Location location = locations[index];
+        return ListTile(
+          leading: Text(
+            location.locationName.substring(0, 1),
+            style: TextStyle(fontSize: 24),
+          ),
+          title: Text(location.locationName),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Machine Name: ${location.machineName}'),
+              Text('Status: ${location.status}'),
+              Text('Description: ${location.description}'),
+            ],
+          ),
+          onTap: () {
+            // Navigate to location details screen
+          },
+        );
+      },
+    );
+  }
+}
+
+class Location {
+  final String locationName;
+  final String machineName;
+  final String status;
+  final String description;
+
+  Location(
+      this.locationName,
+      this.machineName,
+      this.status,
+      this.description,
+      );
+}
+
+
+
+
 class NotificationsScreen extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-return Center(
-child: Icon(Icons.notifications, size: 350),
-);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('Notification List'),
+      // ),
+      body: NotificationListScreen(),
+    );
+  }
 }
+
+class NotificationListScreen extends StatefulWidget {
+  @override
+  _NotificationListScreenState createState() => _NotificationListScreenState();
 }
+
+class _NotificationListScreenState extends State<NotificationListScreen> {
+  CollectionReference notificationsCollection =
+  FirebaseFirestore.instance.collection('notifications');
+
+  List<NotificationItem> notifications = [];
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    loadNotifications();
+  }
+
+  Future<void> loadNotifications() async {
+    if (isLoading) return;
+    setState(() {
+      isLoading = true;
+    });
+
+    // Simulating API call or data retrieval
+    await Future.delayed(Duration(seconds: 2));
+
+    QuerySnapshot querySnapshot = await notificationsCollection
+        .orderBy('notificationDate', descending: true)
+        .get();
+
+    List<NotificationItem> newNotifications = querySnapshot.docs.map((doc) {
+      return NotificationItem(
+        doc['partyName'],
+        doc['notificationDate'],
+        doc['status'],
+        doc['notificationText'],
+      );
+    }).toList();
+
+    setState(() {
+      notifications.addAll(newNotifications);
+      isLoading = false;
+    });
+  }
+
+  Future<void> refreshNotifications() async {
+    notifications.clear();
+    await loadNotifications();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: refreshNotifications,
+      child: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: notifications.length,
+        itemBuilder: (context, index) {
+          NotificationItem notification = notifications[index];
+          return ListTile(
+            leading: Text(
+              notification.partyName.substring(0, 1),
+              style: TextStyle(fontSize: 24),
+            ),
+            title: Text(notification.partyName),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Notification Date: ${notification.notificationDate}'),
+                Text('Status: ${notification.status}'),
+                Text('Notification Text: ${notification.notificationText}'),
+              ],
+            ),
+            onTap: () {
+              // Navigate to notification details screen
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class NotificationItem {
+  final String partyName;
+  final String notificationDate;
+  final String status;
+  final String notificationText;
+
+  NotificationItem(
+      this.partyName,
+      this.notificationDate,
+      this.status,
+      this.notificationText,
+      );
+}
+
