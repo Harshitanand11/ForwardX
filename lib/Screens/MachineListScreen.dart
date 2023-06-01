f66f8dbdeca8b8759d4487bd421be2176d08dd5c
Harshitanand11 <83498313+Harshitanand11@users.noreply.github.com>
2023-06-01 14:17:43 +0530

    swipe right to left

--- lib/Screens/MachineListScreen.dart
+++ lib/Screens/MachineListScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class MachineListScreen extends StatefulWidget {
   @override
   _MachineListScreenState createState() => _MachineListScreenState();
@@ -50,9 +53,9 @@ class _MachineListScreenState extends State<MachineListScreen> {
 
     List<Machine> newMachines = querySnapshot.docs.map((doc) {
       return Machine(
-        doc['machineName'],
-        doc['status'],
-        doc['description'],
+        doc['machineName'] ?? '',
+        doc['status'] ?? '',
+        doc['description'] ?? '',
       );
     }).toList();
 
@@ -89,29 +92,93 @@ class _MachineListScreenState extends State<MachineListScreen> {
                 return SizedBox();
               }
             } else {
-              return ListTile(
-                title: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        machines[index].machineName,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Text(machines[index].status),
-                  ],
+              return Dismissible(
+                key: Key(machines[index].machineName),
+                direction: DismissDirection.horizontal,
+                background: Container(
+                  alignment: Alignment.centerRight,
+                  color: Colors.red,
+                  child: Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                  ),
                 ),
-                subtitle: Text(machines[index].description),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MachineDetailsScreen(
-                        machine: machines[index],
-                      ),
-                    ),
-                  );
+                secondaryBackground: Container(
+                  alignment: Alignment.centerLeft,
+                  color: Colors.green,
+                  child: Icon(
+                    machines[index].status == 'Active'
+                        ? Icons.toggle_off
+                        : Icons.toggle_on,
+                    color: Colors.white,
+                  ),
+                ),
+                confirmDismiss: (direction) {
+                  if (direction == DismissDirection.startToEnd) {
+                    // Toggle machine status to inactive if not already inactive
+                    if (machines[index].status != 'Inactive') {
+                      machinesCollection
+                          .doc(machines[index].machineName)
+                          .update({'status': 'Inactive'});
+                      setState(() {
+                        machines[index].status = 'Inactive';
+                      });
+                    }
+                  } else if (direction == DismissDirection.endToStart) {
+                    // Toggle machine status to active if not already active
+                    if (machines[index].status != 'Active') {
+                      machinesCollection
+                          .doc(machines[index].machineName)
+                          .update({'status': 'Active'});
+                      setState(() {
+                        machines[index].status = 'Active';
+                      });
+                    }
+                  }
+                  return Future.value(false);
                 },
+
+
+                // onDismissed: (direction) {
+                //   // if (direction == DismissDirection.endToStart) {
+                //   //   // Delete machine
+                //   //   machinesCollection
+                //   //       .doc(machines[index].machineName)
+                //   //       .delete();
+                //   //   setState(() {
+                //   //     machines.removeAt(index);
+                //   //   });
+                //     ScaffoldMessenger.of(context).showSnackBar(
+                //       SnackBar(
+                //         content: Text('Machine deleted.'),
+                //       ),
+                //     );
+                //   }
+                // },
+                child: ListTile(
+                  title: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          machines[index].machineName,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(machines[index].status),
+                    ],
+                  ),
+                  subtitle: Text(machines[index].description),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MachineDetailsScreen(
+                          machine: machines[index],
+                        ),
+                      ),
+                    );
+                  },
+                ),
               );
             }
           },
@@ -119,15 +186,12 @@ class _MachineListScreenState extends State<MachineListScreen> {
       ),
     );
   }
-
 }
 
-
-
 class Machine {
-  final String machineName;
-  final String status;
-  final String description;
+  String machineName;
+  String status;
+  String description;
 
   Machine(this.machineName, this.status, this.description);
 }
@@ -185,7 +249,6 @@ class MachineDetailsScreen extends StatelessWidget {
     );
   }
 }
-
 
 
 
