072049d5773e15662bc1ef89fd09bf9e111e7a6c
Harshitanand11 <83498313+Harshitanand11@users.noreply.github.com>
2023-06-19 15:37:38 +0530

    Dahboard screen 2 implemented

--- lib/Screens/DashboardScreen.dart
+++ lib/Screens/DashboardScreen.dart
@@ -1,42 +1,42 @@
-import 'package:flutter/material.dart';
-import 'package:graphic/graphic.dart';
-
-class StackedBarChart extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Create some data for the stacked bar chart.
-    List<DataPoint<int>> data = [
-      DataPoint<int>(x: 1, y: 2, color: Colors.red),
-      DataPoint<int>(x: 2, y: 4, color: Colors.green),
-      DataPoint<int>(x: 3, y: 6, color: Colors.blue),
-      DataPoint<int>(x: 4, y: 8, color: Colors.yellow),
-    ];
-
-    // Create the stacked bar chart.
-    var stackedBarChart = StackedBarChartComponent(
-      data,
-      flipVerticalAxis: true,
-      verticalAxisStep: 2,
-      horizontalAxisStep: 1,
-      barWidth: 20,
-      legend: Legend(
-        position: LegendPosition.top,
-        textStyle: TextStyle(fontSize: 14),
-      ),
-      tooltip: Tooltip(enable: true),
-    );
-
-    // Return the stacked bar chart as a Scaffold widget.
-    return Scaffold(
-      body: Center(
-        child: stackedBarChart,
-      ),
-    );
-  }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: StackedBarChart(),
-  ));
-}
+// import 'package:flutter/material.dart';
+// import 'package:graphic/graphic.dart';
+//
+// class StackedBarChart extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     // Create some data for the stacked bar chart.
+//     List<DataPoint<int>> data = [
+//       DataPoint<int>(x: 1, y: 2, color: Colors.red),
+//       DataPoint<int>(x: 2, y: 4, color: Colors.green),
+//       DataPoint<int>(x: 3, y: 6, color: Colors.blue),
+//       DataPoint<int>(x: 4, y: 8, color: Colors.yellow),
+//     ];
+//
+//     // Create the stacked bar chart.
+//     var stackedBarChart = StackedBarChartComponent(
+//       data,
+//       flipVerticalAxis: true,
+//       verticalAxisStep: 2,
+//       horizontalAxisStep: 1,
+//       barWidth: 20,
+//       legend: Legend(
+//         position: LegendPosition.top,
+//         textStyle: TextStyle(fontSize: 14),
+//       ),
+//       tooltip: Tooltip(enable: true),
+//     );
+//
+//     // Return the stacked bar chart as a Scaffold widget.
+//     return Scaffold(
+//       body: Center(
+//         child: stackedBarChart,
+//       ),
+//     );
+//   }
+// }
+//
+// void main() {
+//   runApp(MaterialApp(
+//     home: StackedBarChart(),
+//   ));
+// }
