5302cbec92fec9fc77a466b5fb4f22add6610c85
Harshitanand11 <83498313+Harshitanand11@users.noreply.github.com>
2023-06-30 13:21:59 +0530

    Linkedin page job description

--- lib/screen/pages/mobile/mobile_screen.dart
+++ lib/screen/pages/mobile/mobile_screen.dart
@@ -1,163 +1,5 @@
 
-// import 'package:flutter/material.dart';
-//
-// class Company {
-//   final String name;
-//   final String description;
-//   final String imageUrl;
-//
-//   Company(this.name, this.description, this.imageUrl);
-// }
-//
-// class CompanyDetailsScreen extends StatelessWidget {
-//   final Company company;
-//
-//   CompanyDetailsScreen(this.company);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Company Details'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Text(
-//               company.name,
-//               style: TextStyle(
-//                 fontSize: 20,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//             SizedBox(height: 16),
-//             Text(
-//               company.description,
-//               style: TextStyle(fontSize: 16),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-// class HomeScreen extends StatelessWidget {
-//   void navigateToCompanyDetails(BuildContext context, Company company) {
-//     Navigator.push(
-//       context,
-//       MaterialPageRoute(
-//         builder: (context) => CompanyDetailsScreen(company),
-//       ),
-//     );
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final companies = [
-//       Company(
-//         'Amazon',
-//         'Amazon.com, Inc. is an American multinational technology company based in Seattle, Washington. It focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence.',
-//         'https://icon2.cleanpng.com/20180320/roq/kisspng-amazon-com-the-exposed-saga-computer-icons-app-sto-amazon-icon-socialmedia-iconset-uiconstock-5ab1b9d01d9a65.8306302815215968801213.jpg',
-//       ),
-//       Company(
-//         'Microsoft',
-//         'Microsoft Corporation is an American multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.',
-//         'https://icon2.cleanpng.com/20180516/jsw/kisspng-start-menu-windows-7-button-microsoft-5afc309aab9321.9654894815264769547028.jpg',
-//       ),
-//       Company(
-//         'Google',
-//         'Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engines, cloud computing, software, and hardware.',
-//         'https://banner2.cleanpng.com/20171216/6c0/google-png-5a3554027e9924.3682726615134443545186.jpg',
-//       ),
-//       Company(
-//         'EY',
-//         'EY (formerly Ernst & Young) is a multinational professional services network with headquarters in London, England. It provides assurance, tax, consulting, and advisory services to companies.',
-//         'https://icon2.cleanpng.com/20180527/zkh/kisspng-ernst-young-accounting-finance-accountant-compan-5b0a8ebaa27f47.7884564315274185546656.jpg',
-//       ),
-//     ];
-//
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Padding(
-//           padding: const EdgeInsets.all(0),
-//           child: TextField(
-//             decoration: InputDecoration(
-//               hintText: 'Search...',
-//               filled: true,
-//               fillColor: Color.fromRGBO(238,243,248,0.9),
-//               prefixIcon: Icon(Icons.search),
-//             ),
-//           ),
-//         ),
-//         backgroundColor: Color.fromRGBO(10,102,194,1),
-//         actions: [
-//           IconButton(
-//             onPressed: () {},
-//             icon: Icon(Icons.more_vert),
-//           ),
-//           IconButton(
-//             icon: Icon(Icons.message),
-//             onPressed: () {
-//               // Handle message action
-//             },
-//           ),
-//         ],
-//       ),
-//       body: ListView.builder(
-//         itemCount: companies.length,
-//         itemBuilder: (context, index) {
-//           final company = companies[index];
-//           return ListTile(
-//             leading: CircleAvatar(
-//               backgroundImage: NetworkImage(company.imageUrl),
-//             ),
-//             title: Text(company.name),
-//             onTap: () => navigateToCompanyDetails(context, company),
-//           );
-//         },
-//       ),
-//       bottomNavigationBar: BottomNavigationBar(
-//         type: BottomNavigationBarType.fixed,
-//         currentIndex: 0,
-//         onTap: (index) {
-//           // Handle tab selection
-//         },
-//         items: [
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.home),
-//             label: 'Home',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.people),
-//             label: 'My Network',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.post_add),
-//             label: 'Post',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.notifications),
-//             label: 'Notifications',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.work),
-//             label: 'Jobs',
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-//
-// void main() {
-//   runApp(MaterialApp(
-//     debugShowCheckedModeBanner: false,
-//     home: HomeScreen(),
-//   ));
-// }
+
 import 'package:flutter/material.dart';
 import 'package:cupertino_icons/cupertino_icons.dart';
 
@@ -173,6 +15,13 @@ class CompanyDetailsScreen extends StatelessWidget {
   final Company company;
 
   CompanyDetailsScreen(this.company);
+  final List<String> otherCompanies = [
+    'Company A',
+    'Company B',
+    'Company C',
+    'Company D',
+    'Company E',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -183,6 +32,7 @@ class CompanyDetailsScreen extends StatelessWidget {
       ),
       body: ListView(
         children: [
+
           ListTile(
             title: Text(
               company.name,
@@ -193,6 +43,7 @@ class CompanyDetailsScreen extends StatelessWidget {
             ),
           ),
           ListTile(
+
             title: Text('Mobile Developer Intern',style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500,),),
           ),
           ListTile(
@@ -206,7 +57,7 @@ class CompanyDetailsScreen extends StatelessWidget {
           ),
           // SizedBox(height: 5),
           ListTile(
-            
+
             title: Row(
               children: [
                 Icon(Icons.shopping_bag),SizedBox(width: 5,),
@@ -282,11 +133,58 @@ class CompanyDetailsScreen extends StatelessWidget {
 
                   ),
                 ),
+
+
+
                 Divider(height: 2.0,
-                    color: Colors.black26)
+                    color: Colors.black26),
+
               ],
             ),
           ),
+          ListTile(
+
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text('Job Description',style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.left,),
+                SizedBox(height: 8),
+                Text('Exp-4to 10 Years',style: TextStyle(fontWeight: FontWeight.w600),),
+                Text('Job:C++Developer'  ),
+                Text('Work Location - Banglore'  ),
+                SizedBox(height: 8),
+                Text('Mandatory Skills:',style: TextStyle(fontWeight: FontWeight.w800),),
+                Text('Working experience on C++ programming language and Linux OS'  ),
+
+              ],
+            ),
+
+          ),
+          ListTile(
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('More jobs:',style: TextStyle(fontWeight: FontWeight.w800)),
+                SizedBox(
+                  height: 60,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: otherCompanies.length,
+                    itemBuilder: (context, index) {
+                      final name = otherCompanies[index];
+                      return Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Chip(
+                          label: Text(name),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          )
+
         ],
       ),
     );
@@ -478,5 +376,6 @@ void main() {
     home: HomeScreen(),
   ));
 }
+
 
 
